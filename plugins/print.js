5cce8c710117b17b07b859fff23022c7977599d5
凉菜 <1659488338@qq.com>
2022-08-06 16:26:20 +0800

    change script

--- plugins/print.js
+++ plugins/print.js
@@ -1,8 +1,19 @@
 const {OitqPlugin} = require("oitq");
 
-const print = new OitqPlugin('print', __filename)
-print.command('print <msg:text>', 'all')
-    .desc('打印一条消息')
+const plugin = new OitqPlugin('common', __filename)
+plugin.command('print <msg:text>', 'all')
+    .desc('打印一条消息11111')
     .action((_, msg) => {
         return msg
     })
+plugin.command('music <keyword:string>','all')
+    .desc('搜索音乐')
+    .desc('快捷方式：来一首 我想听 点歌')
+    .shortcut(/^来一首(.+)$/,{args:['$1'],fuzzy:true})
+    .shortcut(/^我想听(.+)$/,{args:['$1'],fuzzy:true})
+    .shortcut(/^点歌(.+)$/,{args:['$1'],fuzzy:true})
+    .option('platform','-p <platform:string> 音乐平台',{initial:'163'})
+    .action(async ({options},keyword)=>{
+        return `触发点歌，关键词：${keyword},所用平台：${options.platform}`
+    })
+
